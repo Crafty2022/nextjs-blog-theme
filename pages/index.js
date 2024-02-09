e6b9aa4c83d23768a51b0ff07251af713c4be01c
crafty art <142874488+Crafty2022@users.noreply.github.com>
2024-02-09 05:34:55 +0000

    dsgfvsdg

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Index({ posts, globalData }) {
           {globalData.blogTitle}
         </h1>
 
-      <b <a href="https://www.craftyartapp.com/"> Crafty art link </a></b>
+      <b><a href="https://www.craftyartapp.com/templates/invitation"> Crafty art link </a></b>
 
         <ul className="w-full">
           {posts.map((post) => (
